d54ce1a73d760351e1c5178ce4a080d045dfef30
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-03-30 11:55:09 +0200

    added file to rename infection files based on date

---
+++ Code/rename_files.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 10:25:42 2020.
+
+@author: laura
+"""
+
+import os
+import re
+
+os.chdir("../Besmettingen RIVM")
+files = os.listdir()
+
+new_names = []
+for i in files:
+    date = re.search(r"\d{8}", i).group()
+    new_names.append(f"infections_{date}.csv")
+
+for i, _ in enumerate(files):
+    os.rename(files[i], new_names[i])
